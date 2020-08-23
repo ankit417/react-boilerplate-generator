8fe0a935971837f8b6856619ed06813e022fc941
Ankeet <acharya.ankit417@gmail.com>
2020-08-23 19:04:36 +0545

    sass generator

--- scripts/web_script/screen_maker_redux.js
+++ scripts/web_script/screen_maker_redux.js
@@ -87,6 +87,29 @@ export function ${screen.toLowerCase()}Reducer(state = initalState, action) {
           }`;
 };
 
+const sassGenerator = (screen) => {
+    return(
+        `
+//${screen.toUpperCase()} STYLES
+        `
+    )
+}
+const sassExporter = (screen) => {
+    return(
+        `
+@import "./${screen}";
+        `
+    )
+}
+
+const layoutExporter =(screen)=> {
+    return(
+        `
+@import "./${screen}Page/0-${screen}Page";
+        `
+    )
+}
+
 exports.webScreenMaker = (screen) => {
   const CURR_DIR = process.cwd();
   fs.mkdirSync(`${CURR_DIR}/components/${screen}Page`);
@@ -114,4 +137,19 @@ exports.webScreenMaker = (screen) => {
     if (err) throw err;
   });
  
+  fs.mkdirSync(`${CURR_DIR}/sass/Layouts/${screen}Page`);
+ 
+  const sass= `${CURR_DIR}/sass/Layouts/${screen}Page/_${screen}.scss`;
+  fs.appendFile(sass, sassGenerator(screen), (err) => {
+    if (err) throw err;
+  });
+  const sass_layout = `${CURR_DIR}/sass/Layouts/${screen}Page/_0-${screen}Page.scss`;
+  fs.appendFile(sass_layout, sassExporter(screen), (err) => {
+    if (err) throw err;
+  });
+  const layout_sass = `${CURR_DIR}/sass/Layouts/_0-layouts.scss`;
+  fs.appendFile(layout_sass, layoutExporter(screen), (err) => {
+    if (err) throw err;
+  });
+
 };
