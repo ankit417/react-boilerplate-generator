35983fe0aab9257848eeb73d5b3175daac9693ff
Ankeet <acharya.ankit417@gmail.com>
2020-10-20 22:08:51 +0545

    splash fixes

--- scripts/android_script/splash_image.js
+++ scripts/android_script/splash_image.js
@@ -5,33 +5,33 @@ exports.generateSplash = (name) => {
 //mipmap-hdpi
 Jimp.read(name, (err, lenna) => {
   if (err) throw err;
-  lenna.resize(480, 800).write("res/mipmap-hdpi/ic_launcher.png");
+  lenna.resize(480, 800).write("res/mipmap-hdpi/splash.png");
 });
 
 //mipmap-mdpi
 Jimp.read(name, (err, lenna) => {
   if (err) throw err;
-  lenna.resize(320, 480).write("res/mipmap-mdpi/ic_launcher.png");
+  lenna.resize(320, 480).write("res/mipmap-mdpi/splash.png");
 
 });
 
 //mipmap-xhdpi
 Jimp.read(name, (err, lenna) => {
   if (err) throw err;
-  lenna.resize(720, 1280).write("res/mipmap-xhdpi/ic_launcher.png");
+  lenna.resize(720, 1280).write("res/mipmap-xhdpi/splash.png");
 
 });
 
 //mipmap-xxhdpi
 Jimp.read(name, (err, lenna) => {
   if (err) throw err;
-  lenna.resize(960, 1600).write("res/mipmap-xxhdpi/ic_launcher.png");
+  lenna.resize(960, 1600).write("res/mipmap-xxhdpi/splash.png");
 });
 
 //mipmap-xxxhdpi
 Jimp.read(name, (err, lenna) => {
   if (err) throw err;
-  lenna.resize(1280, 1920).write("res/mipmap-xxxhdpi/ic_launcher.png");
+  lenna.resize(1280, 1920).write("res/mipmap-xxxhdpi/splash.png");
 
 });
 }
